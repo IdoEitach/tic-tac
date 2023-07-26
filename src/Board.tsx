520b90fd393a6f015eb51a8a6e660ee7773f665c
IdoEitach <112800537+IdoEitach@users.noreply.github.com>
2023-07-27 00:05:37 +0300

    .

--- src/Board.tsx
+++ src/Board.tsx
@@ -3,19 +3,20 @@ import "./Board.css";
 import { hover } from "@testing-library/user-event/dist/hover";
 
 function BordChange() {
-  const bordSize:number = 3 ; //gives u option to change bord size(bord size is)
+  const bordSize: number = 3; //gives u option to change bord size(bord size is)
   const [board, setBord] = useState(
-    new Array(bordSize).fill("").map(() => new Array(bordSize).fill("") ,String)
+    new Array(bordSize).fill("").map(() => new Array(bordSize).fill(""), String)
   );
-  const [isGameEnd,setGameEnd]=useState(false);
-  const [isTie,setTie] = useState(false);
+
+  const [isGameEnd, setGameEnd] = useState(false);
+  const [isTie, setTie] = useState(false);
   const Player = {//choosing for type of players
     X: "X",
     O: "O",
   };
-  const msgXWon= Player.X +" won";
-  const msgOWon= Player.O +" won";
-  const msgTie=  "tie";
+  const msgXWon = Player.X + " won";
+  const msgOWon = Player.O + " won";
+  const msgTie = "tie";
 
   const [isXTurn, setTurn] = useState(true);
   const [msg, setMsg] = useState("no one won yet");
@@ -50,7 +51,7 @@ function BordChange() {
         for (let i = 0; i < bordSize; i++) {
           if (board[i][i] != turn) break;
           if (i == bordSize - 1) {
-            if (turn ==  Player.X) {
+            if (turn == Player.X) {
               setMsg(msgXWon);
             } else {
               setMsg(msgOWon);
@@ -76,29 +77,31 @@ function BordChange() {
     }
   }
 
-  function click(row:number, coll:number) {
+  function click(row: number, coll: number) {
     const bordToChange = [...board];
-    
-      if (!isGameEnd) {
-        if (board[row][coll] == "") {
-          if (isXTurn) {
-            setTurn(false);
-            bordToChange[row][coll] = Player.X;
-            isWon(row, coll, Player.X);
-          } else {
-            setTurn(true);
-            bordToChange[row][coll] = Player.O;
-            isWon(row, coll, Player.O);
-          }
-          setBord(bordToChange);
-          if(!board.some(row => row.includes(""))){
-            setTie(true);
-            setMsg(msgTie);
-          }
+
+    if (!isGameEnd) {
+      if (board[row][coll] == "") {
+        if (isXTurn) {
+
+          setTurn(false);
+          bordToChange[row][coll] = Player.X;
+          isWon(row, coll, Player.X);
+        } else {
+
+          setTurn(true);
+          bordToChange[row][coll] = Player.O;
+          isWon(row, coll, Player.O);
+        }
+        setBord(bordToChange);
+        if (!board.some(row => row.includes(""))) {
+          setTie(true);
+          setMsg(msgTie);
+        }
       }
     }
   }
-  
+
   return (
     <div>
       {board.map((row, i) => (<div className="container">{row.map((cell, j) => (<button onClick={() => click(i, j)}>{cell}</button>))}</div>))}
